9cd06a3b30959dbb4969d4702652c9ba704378ae
ytworks <yuzo.takagi.private@gmail.com>
2025-04-10 23:43:29 +0900

    refuctring

--- chatmol/__init__.py
+++ chatmol/__init__.py
@@ -2,7 +2,7 @@
 chatMol - Library for calculating molecular properties from molecular structures
 """
 
-from .properties import calculate_molecular_features, get_property_descriptions, get_available_properties, MOLECULAR_FILTERS
+from .properties import calculate_molecular_features, get_property_descriptions, get_available_properties, get_feature_descriptions
 from .io import convert_properties_to_markdown, convert_dataframe_to_markdown, add_properties_to_dataframe
 
 __version__ = "0.1.0"
